901d27c56a1e12bca7e0cdb7fa535e667052ffda
Ayush-Bulbule <ayushbulbule24@gmail.com>
2023-10-07 00:30:07 +0530

    Added Supabase Key, Backend

--- client/src/components/services/supabase.tsx
+++ client/src/components/services/supabase.tsx
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://djsmbnbonxsnntdkenks.supabase.co'
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1dmljeXZnZGZxc2FmaHl5bnB2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY2MTg0NDAsImV4cCI6MjAxMjE5NDQ0MH0.o5mVKPPg7EOXJgKTyX-x0KDnp_nDTgXBPlfWxIIu0nY'
 export const supabase = createClient(supabaseUrl, supabaseKey)
